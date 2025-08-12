c96218b75859f301487b9d4ed1ce41055c85ef71
jeff <alainnnsarmago@gmail.com>
2025-08-12 10:38:31 +0800

    feat: add solition for container with most water

--- two pointers problemn/container-with-most-water.py
+++ two pointers problemn/container-with-most-water.py
@@ -27,3 +27,24 @@ n == height.length
 2 <= n <= 10^5
 0 <= height[i] <= 10^4
 """
+
+
+def max_area(height):
+  left, right = 0, len(height) - 1
+  max_area = 0
+
+  while left < right:
+    width = right - left
+    min_height = min(height[right], height[left])
+    area = width * min_height
+    max_area = max(max_area, area)
+
+    if height[left] < height[right]:
+      left += 1
+    else:
+      right -= 1
+  
+  return max_area
+
+height = [1,8,6,2,5,4,8,3,7]
+print(max_area(height))
